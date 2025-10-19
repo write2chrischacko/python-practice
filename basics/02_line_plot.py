62fee09a17dc49c27425186c6ea2bc4bf185893f
Christina Chacko <write2chrischacko@gmail.com>
2025-10-19 09:22:21 +0530

    add grid lines and legend

--- basics/02_line_plot.py
+++ basics/02_line_plot.py
@@ -4,6 +4,13 @@ import numpy as np
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 
-plt.plot(x, y)
+plt.plot(x, y, linestyle='-')       # OG sine wave
+plt.plot(x, y*0.5, linestyle='--')  # Shifter sine wave
+plt.legend(["Sine", "Sine (dashed)"], loc="upper right")
+
 plt.title("Sine Wave")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+
+plt.grid(color='gray', linestyle='--', linewidth=0.5)
 plt.show()
